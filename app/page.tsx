fa45df81cf04fa4e14f32b74a079618df7f90e6b
learnuidev <learnuidev@gmail.com>
2025-02-03 20:51:56 -0500

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,11 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { useToast } from "@/hooks/use-toast";
 import { useTranslation } from "@/libs/i18n-next/use-translation";
-
-async function copyTextToClipboard(text: string) {
-  if (navigator.clipboard) {
-    // Use the Clipboard API for modern browsers
-    return navigator.clipboard
-      .writeText(text)
-      .then(() => {
-        console.log("Text copied to clipboard");
-        return null;
-      })
-      .catch((err) => {
-        console.error("Failed to copy: ", err);
-      });
-  } else {
-    return null;
-    // Fallback for older browsers
-  }
-}
+import { copyTextToClipboard } from "@/libs/misc/copy-text-to-clipboard";
 
 export default function Home() {
   const { t } = useTranslation(["common"]);
-  const { toast } = useToast();
+
   return (
     <div className="flex items-center justify-center flex-col max-w-4xl m-auto">
       <h1 className="text-center text-5xl mt-32 font-bold font-geist-sans">
@@ -33,16 +14,13 @@ export default function Home() {
       <p className="text-center text-2xl dark:text-gray-400">
         {t("description")}
       </p>
-      <Button className="rounded-none mt-12">{t("startAutomating")}</Button>
+      <h3 className="rounded-none mt-12 text-xl">{t("startAutomating")}</h3>
       <div>
-        <div className="flex mt-12 border border-dashed border-muted-foreground p-2 px-4 text-sm w-full relative">
+        <div className="flex mt-4 border border-dashed border-muted-foreground p-2 px-4 text-sm w-full relative">
           <button
             onClick={() => {
               copyTextToClipboard(`npx myelino --translate`).then(() => {
-                // alert("Copied to clipboard");
-                toast({
-                  description: "Copied to clipboard!",
-                });
+                alert("Copied to clipboard");
               });
             }}
             className="flex items-center space-x-2 overflow-hidden"
