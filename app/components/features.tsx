ae7e19fd7a875d702c631147d7d439bf10b64811
learnuidev <learnuidev@gmail.com>
2025-02-03 21:56:48 -0500

    wip

--- app/components/features.tsx
+++ app/components/features.tsx
@@ -1,7 +1,52 @@
-export const Features = () => {
+import { useTranslation } from "@/libs/i18n-next/use-translation";
+
+function FeatureItem({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) {
   return (
-    <section>
-      <h2>Features</h2>
+    <div>
+      <h4 className="font-bold uppercase font-mono">{title}</h4>
+
+      <p className="font-extralight mt-2 dark:text-gray-400">{description}</p>
+    </div>
+  );
+}
+
+export const Features = () => {
+  const { t } = useTranslation(["features"]);
+
+  return (
+    <section className="my-32">
+      <h2 className="text-2xl mb-8 text-center font-mono uppercase font-bold">
+        {t("feature.title")}
+      </h2>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-12">
+        <FeatureItem
+          title={t("feature.private")}
+          description={t("feature.private.description")}
+        />
+        <FeatureItem
+          title={t("feature.openSource")}
+          description={t("feature.openSource.description")}
+        />
+        <FeatureItem
+          title={t("feature.multiModel")}
+          description={t("feature.multiModel.description")}
+        />
+        <FeatureItem
+          title={t("feature.byom")}
+          description={t("feature.byom.description")}
+        />
+        <FeatureItem
+          title={t("feature.firstClassi18n")}
+          description={t("feature.firstClassi18n.description")}
+        />
+      </div>
     </section>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -2,34 +2,38 @@
 
 import { useTranslation } from "@/libs/i18n-next/use-translation";
 import { copyTextToClipboard } from "@/libs/misc/copy-text-to-clipboard";
+import { Features } from "./components/features";
 
 export default function Home() {
   const { t } = useTranslation(["common"]);
 
   return (
-    <div className="flex items-center justify-center flex-col max-w-4xl m-auto">
-      <p className="text-center text-5xl mt-32 dark:text-gray-200">
-        {t("description")}
-      </p>
-      <h3 className="rounded-none mt-6 dark:text-gray-400 text-center font-mono">
-        {t("banner")}
-      </h3>
-      <div>
-        <div className="flex mt-8 border border-dashed border-muted-foreground p-2 px-4 text-sm w-full relative">
-          <button
-            onClick={() => {
-              copyTextToClipboard(`npx myelino --translate`).then(() => {
-                alert("Copied to clipboard");
-              });
-            }}
-            className="flex items-center space-x-2 overflow-hidden"
-          >
-            <span className="text-primary truncate">
-              npx myelino --translate
-            </span>
-          </button>
+    <div className="max-w-4xl mx-auto p-8">
+      <div className="flex items-center justify-center flex-col m-auto">
+        <p className="text-center text-4xl sm:text-5xl mt-32 dark:text-gray-200">
+          {t("description")}
+        </p>
+        <h3 className="rounded-none mt-6 dark:text-gray-400 max-w-3xl text-center font-mono">
+          {t("banner")}
+        </h3>
+        <div>
+          <div className="flex mt-8 border border-dashed border-muted-foreground p-2 px-4 text-sm w-full relative">
+            <button
+              onClick={() => {
+                copyTextToClipboard(`npx myelino --translate`).then(() => {
+                  alert("Copied to clipboard");
+                });
+              }}
+              className="flex items-center space-x-2 overflow-hidden"
+            >
+              <span className="text-primary truncate">
+                npx myelino --translate
+              </span>
+            </button>
+          </div>
         </div>
       </div>
+      <Features />
     </div>
   );
 }
