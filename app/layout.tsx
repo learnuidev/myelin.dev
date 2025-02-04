fb4616900eaf82c98104f4be22165a4fe151573f
learnuidev <learnuidev@gmail.com>
2025-02-04 13:56:36 -0500

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,12 @@
 import { Footer } from "@/components/footer";
 import { NavBar } from "@/components/navbar/navbar";
 import { ThemeProvider } from "@/components/theme-provider/theme-provider";
-import { I18NextProvider } from "@/libs/i18n-next/i18n-next-provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import { Toaster } from "@/components/ui/toaster";
-import { getLangAndDirection } from "@/libs/i18n-next/get-lang-and-direction";
+import { I18NextHtmlProvider } from "@/libs/i18n-next/i18n-next-html-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,32 +28,28 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { lang, direction } = await getLangAndDirection();
-
   return (
-    <I18NextProvider>
-      <html suppressHydrationWarning lang={lang} dir={direction}>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    <I18NextHtmlProvider>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
         >
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Toaster />
-            <div className="relative">
-              <div className="fixed top-0  w-full z-50">
-                <NavBar />
-              </div>
-              {children}
+          <Toaster />
+          <div className="relative">
+            <div className="fixed top-0  w-full z-50">
+              <NavBar />
             </div>
+            {children}
+          </div>
 
-            <Footer />
-          </ThemeProvider>
-        </body>
-      </html>
-    </I18NextProvider>
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </I18NextHtmlProvider>
   );
 }
